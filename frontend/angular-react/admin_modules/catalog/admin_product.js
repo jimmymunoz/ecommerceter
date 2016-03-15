fdc519043e8c793f8b9673b1758879d621097252
jimmymunoz <myappeu@gmail.com>
2016-03-15 10:15:08 +0100

    Git ignore

--- frontend/angular-react/admin_modules/catalog/admin_product.js
+++ frontend/angular-react/admin_modules/catalog/admin_product.js
@@ -14,45 +14,45 @@ angular.module('admin_product').run(function($rootScope, $location, $routeParams
 		category: '',
 		image: '',
 	};
-    $rootScope.searchProducts = function($event){
-    	var strSearchFormParams = $httpParamSerializer($rootScope.search_admin_product_form);
-    	$http.get (config.pathApiServer + 'product/getProductsList/?' + strSearchFormParams).then(function(response){
-            console.log(response.data);
-            if( response.data.success  ){
-            	$rootScope.product_list_data = response.data.data;
-            }
-            else{
-            	alert(response.data.message);
-            }
-        });
-	}
 });
 
-angular.module('admin_product').controller('AdminCreateProductController', ['$rootScope', '$http', function($rootScope, $http){
-	$rootScope.sendProductManagerForm = function($event){
-		$rootScope.searchProducts();
+angular.module('admin_product').controller('AdminCreateProductController', ['$scope', '$http', function($scope, $http){
+   	$scope.sendProductManagerForm = function(product_manager_form){
+   		console.log(product_manager_form);
+    	$http.post (config.pathApiServer + 'product/createProduct/', $scope.product_manager_form,
+	    		{ headers: {'Content-Type': undefined, 'Access-Control-Allow-Origin': '*'} }
+    		).then(function(response){
+	            console.log(response.data);
+	            if( response.data.success  ){
+	            	$scope.product_list_data = response.data.data;
+	            }
+	            else{
+	            	alert(response.data.errors);
+	            }
+        	}
+        );
 	}
-	$rootScope.getCategorysList = function($event){
+	$scope.getCategorysList = function($event){
 		var categoryFilters = "";
 		$http.get (config.pathApiServer + 'category/getCategorysList/?' + categoryFilters).then(function(response){
             if( response.data.success  ){
-            	$rootScope.search_admin_product_form.category_options = [];
+            	$scope.search_admin_product_form.category_options = [];
             	console.log(response.data.data);
             	for (key in response.data.data){
-            		$rootScope.search_admin_product_form.category_options.push({
+            		$scope.search_admin_product_form.category_options.push({
             			name: response.data.data[key]['name'],
             			value: response.data.data[key]['idCategory'],
             		});
             	}
-            	console.log($rootScope.search_admin_product_form.category_options);
+            	console.log($scope.search_admin_product_form.category_options);
             }
             else{
             	alert(response.data.message);
             }
         });
 	}
-	//$rootScope.getCategorysList();
-	//$rootScope.searchProducts();
+	//$scope.getCategorysList();
+	//$scope.searchProducts();
 }]);
 
 angular.module('admin_product').directive('adminProductManager', function(){
